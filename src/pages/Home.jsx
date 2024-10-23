e834662bf443e9c03d8e98394699986affa1e49e
julietaaaaaaaaaaaa <julia.souza3@estudante.ifms.edu.br>
2024-10-23 09:37:20 -0400

    uuuuu

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import ListarProdutos from "./ListarProdutos";
+export default function Home(){
+
+    const [listaHome, setLista] = useState([]);
+
+    useEffect(() => {
+        const ReceberListaProdutos = async () => {
+            try {
+                const resposta = await fetch('https://fakestoreapi.com/products');
+
+                const dados = await resposta.json();
+                
+                setLista(dados);
+            } catch (erro) {
+                console.error("Opssss, bugouuu :D", erro);
+            }
+        };
+        ReceberListaProdutos();
+    }, []);
+    return (
+        <>
+        <ListarProdutos lista ={listaHome}/>
+        </>
+    )
+
+}
+
+
