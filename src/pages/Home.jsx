bbc1626717f7d2c0029f108e03bac83cd9f07027
Julietaa <julia.souza3@estudante.ifms.edu.br>
2024-11-27 10:35:16 -0400

    amolet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,18 +22,39 @@ export default function Home(){
     }, []);
 
     const orderAZ = () => {
-    const listaAux = [...lista].sort((a, b)=> a.title.localeCompare(b.title));
+    const listaAux = [...listaHome].sort((a, b)=> a.title.localeCompare(b.title));
     setLista(listaAux);
 }
-    const orderZA = () => {
-    const listaAux = [...lista].reverse((a, b)=> a.title.localeCompare(b.title));
+
+const orderZA = () => {
+    const listaAux = [...listaHome].sort((a, b) => b.title.localeCompare(a.title)); // Ordenação de Z a A
     setLista(listaAux);
+};
+//menor para o maior
+const precoOrdenadoMenor = () => {
+    const filtro = [...listaHome].sort((a, b)  => a.price - b.price);
+    setLista(filtro);
 }
+//maior para o menor
+const precoOrdenadoMaior = () => {
+    const filtro = [...listaHome].sort((a, b)  => b.price - a.price);
+    setLista(filtro);
+}
+ const PesquisarProduto =(valor)=>{
+    const listaAux = listaHome.filter((produto) =>
+    produto.title.toLowerCase().includes(valor.toLowerCase())
+);
+setLista(listaAux);
+
+ }
     return (
         <>
         <h1>Lista de produtos</h1>
         <button onClick={()=>orderAZ()}>Az</button>
-        <button onClick={()=>orderZA}>Za</button>
+        <button onClick={()=>orderZA()}>Za</button>
+        <button onClick={()=>precoOrdenadoMenor()}>Menor</button>
+        <button onClick={()=>precoOrdenadoMaior()}>Maior</button>
+        <input className="pesquisa"placeholder="Pesquisar..." onChange={(item)=> PesquisarProduto(event.target.value)}/>
 
         <ListarProdutos lista ={listaHome}/>
         </>
